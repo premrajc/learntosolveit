f8a1d5320a9b14000e97386a5e5b72a058ce5ca0
premrajc <34774493+premrajc@users.noreply.github.com>
2017-12-22 21:37:10 +0530

    Update Ex_6.5.c
    Adding a alternative solution for Ex_6.5. 
    Forgive me if I do something wrong, I m newbie here.

--- languages/cprogs/Ex_6.5.c
+++ languages/cprogs/Ex_6.5.c
@@ -1,132 +1,174 @@
-/* Write a function undef that will remove a name and definition
- * from the table maintained by lookup and install.
- */
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
-#include<stdlib.h>
-#include<stdio.h>
+// Keeping hashtable size smaller value to 10.
+#define HASHSIZE 10
 
-
-/*undef will be if it is just hashtable. Remove it.
- * If it is in linked list, delete from linked list.
- */
-
-/* nlist from K&R Page 144 */
-
-struct nlist { 				/* table entry: */
-	struct nlist *next; 	/* next entry in chain */
-	char *name;				/* defined name */
-	char *defn;				/* replacement text */
+//Sturtcure of node.
+struct nlist {
+    struct nlist *next;
+    char *name;
+    char *defn;
 };
 
+// Array of pointer to the nodes.
+static struct nlist *hashtab[HASHSIZE];
 
-#define HASHSIZE 101
-
-static struct nlist *hashtab[HASHSIZE];	/* pointer table */
-
-/* hash: form hash value for string s */
-unsigned hash(char *s)
-{
-	unsigned hashval;
-
-	for(hashval = 0; *s != '\0'; s++)
-		hashval = *s + 31 * hashval;
-
-	return hashval % HASHSIZE;
+//Hash function to get index value of hashtab[].
+unsigned hash(char *s) {
+    unsigned hashval;       // hashval is index in hashtab[].
+    for(hashval=0; *s!='\0'; s++)
+        hashval = *s + 31 * hashval;
+    return hashval % HASHSIZE;
 }
 
-
-/* lookup: look for s in hashtab */
-
-struct nlist *lookup(char *s)
-{
-	struct nlist *np;
-
-	for (np = hashtab[hash(s)]; np != NULL; np = np->next)
-		if (strcmp(s, np->name) == 0)
-			return np;	/* found */
-	return NULL;		/* not found */
+//Function to look for the specific name in the hashtab[].
+struct nlist *lookup(char *s) {
+    struct nlist *np;
+	// for loop, in case there are multinode on single index of hashtab.
+    for(np=hashtab[hash(s)]; np != NULL; np = np -> next)
+        if(strcmp(s, np ->name) == 0)
+            return np;
+    return NULL;
 }
 
-struct nlist *lookup(char *);
-char *strdup(char *);
-
-/* install: put (name, defn) in hashtab */
-struct nlist *install(char *name, char *defn)
-{
-	struct nlist *np;
-	unsigned hashval;
-
-	if ((np = lookup(name)) == NULL)  {	/* not found */
-		np = (struct nlist *) malloc(sizeof(*np));
-
-		if (np == NULL || (np->name = strdup(name)) == NULL)
-			return NULL;
-		hashval = hash(name);
-		np->next = hashtab[hashval];
-		hashtab[hashval] = np;
-	} else 	/* already there */
-		free((void *) np->defn);	/* free the previous defn */
-
-	if ((np->defn = strdup(defn)) == NULL)
-		return NULL;
-
-	return np;
+/*
+	Function to add new node to the hashtab[], if already exist then simply 
+	changes the 'defn' value of that node.
+*/
+struct nlist *install(char *name, char *defn) {
+    struct nlist *np;
+    unsigned hashval;
+    if((np = lookup(name)) == NULL) {
+        np = (struct nlist *) malloc(sizeof(struct nlist));
+        if (np == NULL || (np -> name =strdup(name)) == NULL)
+            return NULL;
+        hashval = hash(name);
+        np -> next = hashtab[hashval];
+        hashtab[hashval] = np;
+    } else
+        free((void *) np -> defn);
+    if((np -> defn = strdup(defn)) == NULL)
+        return NULL;
+    return np;
 }
 
-struct nlist *undef(char *name) {
-	struct nlist *found;
-
-	found = lookup(name);
-
-	if (found == NULL) /* not found and nothing to do */
-		return NULL;
-	else {
-		if (found->next != NULL) {
-			found->next = found->next->next;
-			found = found->next;
-		} else {
-			hashtab[hash(name)] = NULL;
-			free((void *) found);
-		}
-	}
-	return found;
+/* 
+	Function to remove entry from hashtab[], given by 'name'.
+	Prints error message if node not found else deletes the entry from list.
+*/
+void undef(char *name) {
+    struct nlist *np, *temp;
+    unsigned hashval;
+    if((np = lookup(name)) == NULL) {
+        printf("No pair found with name %s.\n", name);
+        return;
+    }
+    hashval = hash(name);
+    temp = hashtab[hashval];
+    if(temp == np) {
+        hashtab[hashval] = np -> next;
+        free(np);
+        return;
+    }
+    while(temp -> next != np)
+        temp = temp -> next;
+    temp -> next = np -> next;
+    free(np);
 }
 
+/*
+	Function to print hashtab[], funtion is being called everytime the undef 
+	function is called.
+*/
+void print_hashtable() {
+    int i;
+    struct nlist *np;
+    for(i=0; i<HASHSIZE; i++) {
+        if(hashtab[i] == NULL)
+            printf("hashtab[%d] : NULL\n", i);
+        else {
+            np = hashtab[i];
+            printf("hashtab[%d] : ", i);
+            while (np -> next != NULL) {
+                    printf("[(%s, %s)] --> ", np -> name, np -> defn);
+                    np = np -> next;
+            }
+            printf("[(%s, %s)]\n", np -> name, np -> defn);
+        }
+    }
+}
 
-int main(int argc, char *argv[])
-{
-	struct nlist *table[4] = {
-			(install("key", "value")),
-			(install("key1", "value1")),
-			(install("key2", "value2")),
-			(install("key3", "value3"))
-	};
+//Function to free up all memory used by malloc().
+void free_hashtable_memory() {
+    int i;
+    struct nlist *np, *temp;
+    for(i=0; i<HASHSIZE; i++) {
+        if(hashtab[i] != NULL) {
+            np = hashtab[i];
+            while(np -> next != NULL) {
+                temp = np -> next;
+                free(np);
+                np = temp;
+            }
+            free(np);
+            hashtab[i] = NULL;
+        }
+    }
+}
 
-	int i;
-
-	for (i=0; i < 4; i++) {
-		printf("%s->%s\n", table[i]->name, table[i]->defn);
-	}
-
-	undef("key");
-	undef("key3");
-
-	struct nlist *result;
-
-	char *keys[4] = {
-			"key",
-			"key1",
-			"key2",
-			"key3"
-	};
-
-	for (i = 0; i < 4; i++) {
-		if ((result = lookup(keys[i])) == NULL) {
-			printf("key not found\n");
-		} else {
-			printf("%s->%s\n", result->name, result->defn);
-		}
-	}
-
-	return 0;
+// main function.
+int main() {
+    int i;
+    char key[10], val[10], choice;
+    struct nlist *table[HASHSIZE] = {0};
+    printf("Enter the key value pair to create hash table(maximum 10 pairs):\n");
+    for(i=0; i<HASHSIZE; i++) {
+        printf("Enter key: ");
+        fpurge(stdin);
+        fgets(key, 11, stdin);
+        key[strlen(key)-1] = '\0';
+        printf("Enter val: ");
+        fpurge(stdin);
+        fgets(val, 11, stdin);
+        val[strlen(val)-1] = '\0';
+        table[i] = install(key, val);
+        while(1) {
+            printf("Want ot enter more...? Press y to continue or n to exit.\n");
+            fpurge(stdin);
+            scanf("%c", &choice);
+            if(choice == 'y' || choice == 'n')
+                break;
+            else
+                printf("Invalid choice.\n");
+        }
+        if(choice == 'n')
+            break;
+    }
+    printf("\nHash Table: \n");
+    print_hashtable();
+    while(1) {
+        printf("\nEnter the key to deleted : ");
+        fpurge(stdin);
+        fgets(key, 11, stdin);
+        key[strlen(key)-1] = '\0';
+        undef(key);
+        printf("\nNew hashtable:\n");
+        print_hashtable();
+        while(1) {
+            printf("\nWant to delete more..? Press y to continue or n to exit.\n");
+            fpurge(stdin);
+            scanf("%c", &choice);
+            if(choice == 'y' || choice == 'n')
+                break;
+            else
+                printf("Invalid choice.\n");
+        }
+        if(choice == 'n')
+            break;
+    }
+    free_hashtable_memory();
+    return 0;
 }
